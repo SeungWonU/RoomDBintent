cbd5df336f01c8be97b6a16b77166de068791c4a
seungwon <ysw8160@naver.com>
2020-08-11 13:20:56 +0900

    change intent to case

--- app/src/main/java/org/techtown/roomdatabaseprac/MainActivity.java
+++ app/src/main/java/org/techtown/roomdatabaseprac/MainActivity.java
@@ -8,7 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener
+{
     public static MyDatabase myDatabase;
     private Button adddata,readdata,deletedata,updatedata;
 
@@ -17,46 +18,42 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myDatabase = Room.databaseBuilder(getApplicationContext(),MyDatabase.class,"infodb").allowMainThreadQueries().build();
+        myDatabase = Room.databaseBuilder(getApplicationContext(), MyDatabase.class, "infodb").allowMainThreadQueries().build();
 
-        adddata =(Button)findViewById(R.id.button_add);
-        readdata=(Button)findViewById(R.id.button_read);
-        deletedata=(Button)findViewById(R.id.button_delete);
-        updatedata=(Button)findViewById(R.id.button_update);
+        adddata = (Button) findViewById(R.id.button_add);
+        readdata = (Button) findViewById(R.id.button_read);
+        deletedata = (Button) findViewById(R.id.button_delete);
+        updatedata = (Button) findViewById(R.id.button_update);
 
-        adddata.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        adddata.setOnClickListener(this);
+        readdata.setOnClickListener(this);
+        deletedata.setOnClickListener(this);
+        updatedata.setOnClickListener(this);
+
+    }
+    public void onClick(View v){
+        int id = v.getId();
+
+        switch (id){
+            case R.id.button_add:{
                 Intent intent =new Intent(MainActivity.this,ADDdataAct.class);
                 startActivityForResult(intent,101);
             }
-        });
-        readdata.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            case R.id.button_read:{
                 Intent intent2= new Intent(MainActivity.this,ReaddataAct.class);
-               // startActivityForResult(intent2,102);
-            startActivity(intent2);
+                startActivity(intent2);
             }
-        });
-    deletedata.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent3= new Intent(MainActivity.this,DeletedataAct.class);
-            startActivityForResult(intent3,103);
-        }
-    });
-        updatedata.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            case R.id.button_delete:{
+                Intent intent3= new Intent(MainActivity.this,DeletedataAct.class);
+                startActivityForResult(intent3,103);
+            }
+            case R.id.button_update:{
                 Intent intent4  = new Intent(MainActivity.this,UpdatedataAct.class);
                 startActivityForResult(intent4,104);
 
-
             }
-        });
+        }
     }
-
 }
 
 
